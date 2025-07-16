ae72bf1ac6d12d71e4b1937ebf116fe3ef0ed2ce
CodeKnight314 <tang.ri@northeastern.edu>
2025-07-16 00:31:54 -0700

    Remove noise scheduler and integrate normalizer calculaitons

--- src/agent.py
+++ src/agent.py
@@ -45,14 +45,7 @@ class PandaAgent():
             raise ValueError(f"[ERROR] Invalid Buffer type. Received {self.config.buffer_type}.")
         
         self.noise_std = self.config.noise_std
-        self.noise_std_start = self.config.noise_std
-        self.noise_std_min = self.config.noise_std_min
-        self.noise_std_end = self.config.noise_std_end
-        
         self.noise_clamp = self.config.noise_clamp
-        self.noise_clamp_start = self.config.noise_clamp
-        self.noise_clamp_min = self.config.noise_clamp_min
-        self.noise_clamp_end = self.config.noise_clamp_min
         
         self.beta = self.config.beta
         self.beta_start = self.config.beta
@@ -65,6 +58,7 @@ class PandaAgent():
         self.ac_update_freq = self.config.ac_update_freq
         self.ac_smoothing_coeff = self.config.smooth_coeff
         self.grad_clip = self.config.grad_clip
+        self.tau = self.config.tau
         
         if weights: 
             self.actor.load(os.path.join(weights, "actor.pth"), self.device)
@@ -73,8 +67,12 @@ class PandaAgent():
             self.target_actor.load_state_dict(self.actor.state_dict())
             self.target_critic_1.load_state_dict(self.critic_1.state_dict())
             self.target_critic_2.load_state_dict(self.critic_2.state_dict())
+        else:
+            self.target_actor.load_state_dict(self.actor.state_dict())
+            self.target_critic_1.load_state_dict(self.critic_1.state_dict())
+            self.target_critic_2.load_state_dict(self.critic_2.state_dict())
         
-    def update_target_network(self, hard_update: bool = True, tau: float = 0.05):
+    def update_target_network(self, hard_update: bool = True, tau: float = 0.005):
         if hard_update: 
             self.target_actor.load_state_dict(self.actor.state_dict())
             self.target_critic_1.load_state_dict(self.critic_1.state_dict())
@@ -88,19 +86,6 @@ class PandaAgent():
                 
             for target_param, param in zip(self.target_critic_2.parameters(), self.critic_2.parameters()):
                 target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
-    
-    def noise_scheduler(self, step: int):
-        ratio = step / self.noise_std_end
-        self.noise_std = max(
-            self.noise_std_min,
-            self.noise_std_start - ratio * (self.noise_std_start - self.noise_std_min)
-        )
-
-        ratio = step / self.noise_clamp_end
-        self.noise_clamp = max(
-            self.noise_clamp_min,
-            self.noise_clamp_start - ratio * (self.noise_clamp_start - self.noise_clamp_min)
-        )
 
     def beta_scheduler(self, step: int):
         ratio = step / self.beta_end
@@ -109,6 +94,15 @@ class PandaAgent():
             self.beta_start + ratio * (self.beta_max - self.beta_start)
         )
         
+    def get_gradient_norm(self, model: torch.nn.Module):
+        total_norm = 0.0
+        for p in model.parameters():
+            if p.grad is not None:
+                param_norm = p.grad.data.norm(2)
+                total_norm += param_norm.item() ** 2
+        total_norm = total_norm ** 0.5
+        return total_norm
+
     def actor_update(self, states: torch.Tensor):
         actions = self.actor(states)
         actor_loss = -self.critic_1(torch.cat([states, actions], dim=-1)).mean()
@@ -116,11 +110,13 @@ class PandaAgent():
         self.actor_opt.zero_grad()
         actor_loss.backward()
         torch.nn.utils.clip_grad_norm_(self.actor.parameters(), max_norm=self.grad_clip)
+        actor_grad_norm = self.get_gradient_norm(self.actor)
+
         self.actor_opt.step()
         
         self.actor_scheduler.step()
         
-        return actor_loss.item()
+        return actor_loss.item(), actor_grad_norm
     
     def critic_update(self, state: torch.Tensor, action: torch.Tensor, reward: torch.Tensor, next_state: torch.Tensor, done: torch.Tensor, weights: torch.Tensor=None):
         with torch.no_grad():
@@ -144,47 +140,54 @@ class PandaAgent():
         
         self.critic_1_opt.zero_grad()
         if weights is not None and weights.numel() > 0:
-            critic_1_loss = torch.nn.functional.mse_loss(current_q1_value, target, reduction='none')
+            critic_1_loss = torch.nn.functional.smooth_l1_loss(current_q1_value, target, reduction='none')
             critic_1_loss = (weights * critic_1_loss).mean()
         else: 
-            critic_1_loss = torch.nn.functional.mse_loss(current_q1_value, target)
+            critic_1_loss = torch.nn.functional.smooth_l1_loss(current_q1_value, target)
         critic_1_loss.backward()
         torch.nn.utils.clip_grad_norm_(self.critic_1.parameters(), self.grad_clip)
+        critic_1_grad_norm = self.get_gradient_norm(self.critic_1)
         self.critic_1_opt.step()
         
         self.critic_2_opt.zero_grad()
         if weights is not None and weights.numel() > 0:
-            critic_2_loss = torch.nn.functional.mse_loss(current_q2_value, target, reduction='none')
+            critic_2_loss = torch.nn.functional.smooth_l1_loss(current_q2_value, target, reduction='none')
             critic_2_loss = (weights * critic_2_loss).mean()
         else: 
-            critic_2_loss = torch.nn.functional.mse_loss(current_q2_value, target)
+            critic_2_loss = torch.nn.functional.smooth_l1_loss(current_q2_value, target)
         critic_2_loss.backward()
         torch.nn.utils.clip_grad_norm_(self.critic_2.parameters(), self.grad_clip)
+        critic_2_grad_norm = self.get_gradient_norm(self.critic_2)
         self.critic_2_opt.step()
         
         self.critic_1_scheduler.step()
         self.critic_2_scheduler.step()
         
+        td_error_1 = torch.abs(current_q1_value - target).detach()
+        td_error_2 = torch.abs(current_q2_value - target).detach()
+        
+        q_value = torch.cat([current_q1_value, current_q2_value], dim=-1).mean().detach().cpu().item()
+
         if weights is not None and weights.numel() > 0:
-            td_error_1 = torch.abs(current_q1_value - target).detach().cpu().numpy()
-            td_error_2 = torch.abs(current_q2_value - target).detach().cpu().numpy()
-            td_error = np.maximum(td_error_1, td_error_2)
-            return critic_1_loss.item(), critic_2_loss.item(), td_error
-        else: 
-            return critic_1_loss.item(), critic_2_loss.item()
+            td_error = torch.maximum(td_error_1, td_error_2).cpu().numpy()
+            return critic_1_loss.item(), critic_2_loss.item(), td_error, q_value, critic_1_grad_norm, critic_2_grad_norm
+        else:
+            td_error = torch.mean(torch.maximum(td_error_1, td_error_2)).cpu().numpy()
+            return critic_1_loss.item(), critic_2_loss.item(), td_error, q_value, critic_1_grad_norm, critic_2_grad_norm
 
     def select_action(self, obs_tensor: np.array, eval_action: bool=False):
+        self.set_eval()
         if not eval_action:
             obs_tensor = torch.as_tensor(obs_tensor, dtype=torch.float32).to(self.device)
             with torch.no_grad(): 
-                action = self.actor(obs_tensor).detach().cpu().numpy()
-            action += np.random.normal(0, self.noise_std, size=action.shape)
-            action = np.clip(action, -1, 1)
+                action = torch.tanh(self.actor(obs_tensor)).detach().cpu().numpy()
+            noise = np.random.normal(0, self.noise_std, size=action.shape)
+            action = np.clip(action + noise, -1, 1)
             return action
         else: 
             obs_tensor = torch.as_tensor(obs_tensor, dtype=torch.float32).to(self.device)
             with torch.no_grad():
-                return self.actor(obs_tensor).cpu().numpy()
+                return np.clip(torch.tanh(self.actor(obs_tensor)).cpu().numpy(), -1, 1)
             
     def push(self, state, action, reward, next_state, done):
         self.buffer.push(state, action, reward, next_state, done)
@@ -193,21 +196,22 @@ class PandaAgent():
         self.buffer.push(idx, state, action, next_state, reward, done, desired_goal, achieved_goal)
         
     def update(self, step: int):
+        self.set_train()
         if isinstance(self.buffer, PERBuffer) or isinstance(self.buffer, PERBufferSumTree):
             states, actions, rewards, next_states, dones, weights, indices = self.buffer.sample(self.batch_size, self.beta)
-            q1_loss, q2_loss, td_error = self.critic_update(states, actions, rewards, next_states, dones, weights) 
+            q1_loss, q2_loss, td_error, q_value, critic_1_grad, critic_2_grad = self.critic_update(states, actions, rewards, next_states, dones, weights) 
             self.buffer.update_priorities(indices=indices, priorities=td_error)
         else: 
             states, actions, rewards, next_states, dones = self.buffer.sample(self.batch_size)
-            q1_loss, q2_loss = self.critic_update(states, actions, rewards, next_states, dones)
+            q1_loss, q2_loss, td_error, q_value, critic_1_grad, critic_2_grad = self.critic_update(states, actions, rewards, next_states, dones)
             
-        self.update_target_network(hard_update=False)
         self.beta_scheduler(step)
+        self.update_target_network(hard_update=False, tau=self.tau)
         if step % self.ac_update_freq == 0:
-            ac_loss = self.actor_update(states)
-            return q1_loss, q2_loss, ac_loss
+            ac_loss, ac_grad = self.actor_update(states)
+            return q1_loss, q2_loss, ac_loss, td_error, q_value, critic_1_grad, critic_2_grad, ac_grad
         else: 
-            return q1_loss, q2_loss
+            return q1_loss, q2_loss, td_error, q_value, critic_1_grad, critic_2_grad
             
     def save_weights(self, path: str):
         self.actor.save(os.path.join(path, "actor.pth"))
@@ -216,7 +220,40 @@ class PandaAgent():
         
     def is_buffer_filled(self):
         return len(self.buffer) >= self.batch_size
+
+    def set_train(self):
+        self.actor.train()
+        self.critic_1.train()
+        self.critic_2.train()
+        self.target_actor.eval()
+        self.target_critic_1.eval() 
+        self.target_critic_2.eval()
+
+    def set_eval(self): 
+        self.actor.eval()
+        self.critic_1.eval()
+        self.critic_2.eval() 
+        self.target_actor.eval()
+        self.target_critic_1.eval() 
+        self.target_critic_2.eval()
+    
+    def update_normalizers(self, obs_list, dg_list):
+        if obs_list:
+            combined_obs = np.concatenate(obs_list, axis=0)
+            self.buffer.obs_normalizer.update(combined_obs)
+        if dg_list:
+            combined_dg = np.concatenate(dg_list, axis=0)
+            self.buffer.dg_normalizer.update(combined_dg)
+    
+    def normalize_state_batch(self, obs_batch, dg_batch):
+        normalized_obs = self.buffer.obs_normalizer.normalize(obs_batch)
+        normalized_dg = self.buffer.dg_normalizer.normalize(dg_batch)
+        return np.concatenate([normalized_obs, normalized_dg], axis=-1)
     
     def reset(self):
         self.actor.reset()
         self.target_actor.reset()
+        self.critic_1.reset()
+        self.critic_2.reset() 
+        self.target_critic_1.reset() 
+        self.target_critic_2.reset()
